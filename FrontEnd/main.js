3798b59a8d844e5ec634344539b439ecf212f1ac
mathct <mathieu.chatrain@gmail.com>
2024-06-26 17:13:09 +0200

    first commit

---
+++ FrontEnd/main.js
@@ -0,0 +1,138 @@
+//recuperation et stockage des données works sur l'api
+const reponse = await fetch('http://localhost:5678/api/works');
+const works = await reponse.json();
+//recuperation et stockage des données categories sur l'api
+const reponse2 = await fetch('http://localhost:5678/api/categories');
+const category = await reponse2.json();
+
+
+
+
+//fonction pour afficher la liste complete des works
+function afficherWorks(works){
+    for (let i = 0; i < works.length; i++) {
+
+        const work = works[i];
+        // Récupération de l'élément du DOM qui accueillera les "works"
+        const groupWork = document.querySelector(".gallery"); 
+        // Création d’une balise dédiée aux "works"
+        const workElement = document.createElement("figure"); 
+        // Création des img (lien + alt) 
+        const imageWork = document.createElement("img");
+        imageWork.src = work.imageUrl;
+        imageWork.alt = work.title;
+        // Création des titres en "figcaption" afin de respecter le html
+        const titreWork = document.createElement("figcaption");
+        titreWork.innerText = work.title;
+
+        // On rattache le workElement (figures) au groupWork (gallery)
+        groupWork.appendChild(workElement);
+        // On rattache les deux elements (img et title) au workElement (figures) 
+        workElement.appendChild(imageWork);
+        workElement.appendChild(titreWork);
+    }
+}
+
+// et on execute la fonction
+afficherWorks(works);
+
+
+
+
+
+
+//creation dynamique des filtres à partir des categories existantes sur l'API 
+
+/*
+let tableau = [];
+// Parcours de la liste des travaux "works" et creation d'un tableau avec les objets "category" de chaque "work"
+for (let i = 0; i < category.length; i++) {
+    const Element = category[i];
+    tableau.push(Element);
+}*/
+
+// Extraction des noms uniques des catégories 
+const uniqueNamesSet = new Set(category.map(item => item.name));
+// Transformation en tableau pour faire le sort et avoir un resultat en tableau
+const uniqueNamesArray = Array.from(uniqueNamesSet);
+// Tri des noms uniques par ordre décroissant d'ID associé
+const listeNames = uniqueNamesArray.sort((nameA, nameB) => {
+    // Trouver les IDs associés aux noms pour le tri
+    const idA = category.find(item => item.name === nameA).id;
+    const idB = category.find(item => item.name === nameB).id;
+    // Comparaison pour le tri décroissant
+    return idA - idB;
+});
+
+if (listeNames.length != 0)
+{
+    listeNames.unshift('Tous');
+}
+
+
+
+// on supprime les boutons
+const group = document.getElementById("filtres");
+group.innerHTML = "";
+
+// creation des boutons
+for (let i=0; i<listeNames.length; i++)
+    {
+        const bouton = document.createElement("div");
+        bouton.id = "bouton_"+i;
+        bouton.innerText = listeNames[i];
+        group.appendChild(bouton);
+        var element = document.getElementById('bouton_'+i);
+        element.classList.add('filtre');
+        if (i==0)
+            {
+                element.classList.add('filtre_actif');
+            }
+    }
+
+// Creation des EventListener
+// on recupere tous les boutons avec la classe filtre
+const buttons = document.querySelectorAll('.filtre');
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function () {
+        
+        if(i==0)
+            {
+                document.querySelector(".gallery").innerHTML = "";
+                for (let j=0; j<listeNames.length; j++)
+                {
+                    var element = document.getElementById('bouton_'+j);
+                    if (element.classList.contains('filtre_actif'))
+                        {
+                            element.classList.remove('filtre_actif');
+                        }
+                        
+                }
+                document.querySelector("#bouton_"+i).classList.add('filtre_actif');
+                afficherWorks(works);
+            }
+
+        else
+        {
+            
+                const filtrework = works.filter(objet=> objet.categoryId == i);
+                document.querySelector(".gallery").innerHTML = "";
+                for (let j=0; j<listeNames.length; j++)
+                    {
+                        var element = document.getElementById('bouton_'+j);
+                        if (element.classList.contains('filtre_actif'))
+                            {
+                                element.classList.remove('filtre_actif');
+                            }
+                            
+                    }
+                document.querySelector("#bouton_"+i).classList.add('filtre_actif');
+                afficherWorks(filtrework);
+            
+
+        }
+
+    });
+}
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# Portfolio-architecte-sophie-bluel
+
+Code du projet 3 de developpeur web.
+
+## Information pour lancer le code
+
+ - Lancer le backend depuis votre terminal en suivant les instruction du fichier ReadMe.
+ - Si vous désirez afficher le code du backend et du frontend, faites le dans 2 instances de VSCode différentes pour éviter tout problème
